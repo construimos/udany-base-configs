82c453af08a2153c2d108c7cd67e4a1aaf14fe14
udany <long.eagle.goodies@gmail.com>
2021-11-03 21:19:45 -0300

    Revert "further testing wtf"
    This reverts commit 6f938b74

--- modules/babel.js
+++ modules/babel.js
@@ -1,20 +1,12 @@
 const babelRegister = require('@babel/register');
-const deepMerge = require('udany-toolbox/helpers/deepMerge.js');
 
 /**
  * Utility wrapper over @babel/register
  * Must be called before any attempts to use modern syntax within node
  * @param babelOptions
  */
-function startBabelRegister(babelOptions) {
-	babelRegister(deepMerge(
-		{ extensions: ['.js', '.ts'] },
-		getBabelOptions(babelOptions)
-	));
-}
-
-function getBabelOptions(babelOptions) {
-	return deepMerge({
+module.exports = function startBabelRegister(babelOptions) {
+	babelRegister({
 		presets: [
 			['@babel/preset-env', {
 				targets: {
@@ -32,11 +24,7 @@ function getBabelOptions(babelOptions) {
 				legacy: true
 			}]
 		],
-
-	}, babelOptions);
-}
-
-module.exports = {
-	getBabelOptions,
-	startBabelRegister
+		extensions: ['.js', '.ts'],
+		...babelOptions
+	});
 }
